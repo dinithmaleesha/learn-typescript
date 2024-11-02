a0e9de43504a8ccd9dc0d35490d8af51d0d38fb0
Dinith Maleesha <maleesha.live123@gmail.com>
2024-11-02 17:59:36 +0530

    lesson-10

---
+++ lesson-10/index.ts
@@ -0,0 +1,32 @@
+// ---------------------
+// type aliases
+// ---------------------
+
+// example 1 - tuple
+type Rgb = [number, number, number]
+
+function getRandomColor(): Rgb {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+
+    return [r,g,b]
+}
+
+const colorOne = getRandomColor()
+console.log(colorOne);
+
+// example 2 - onject literal
+type User = {
+    name: string,
+    score: number
+}
+const userOne: User = {
+    name: 'Dinith',
+    score: 75
+}
+
+function formatUser(user: User): void {
+    console.log(`${user.name} has a score of ${user.score}`);
+}
+formatUser(userOne)
