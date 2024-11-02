d7e0847f8dd005693fd2e170cb067c65c2649f55
Dinith Maleesha <maleesha.live123@gmail.com>
2024-11-02 17:51:28 +0530

    lesson-9

--- lesson-9/index.ts
+++ lesson-9/index.ts
@@ -1,1 +1,39 @@
-// lesson #09
+// ---------------
+// interfaces
+// ---------------
+interface Author {
+    name: string,
+    avatar: string
+}
+const authorOne: Author = {name: 'Dinith', avatar: 'img.png'}
+
+interface Post {
+    title: string,
+    body: string,
+    tags: string[],
+    create_at: Date,
+    author: Author
+}
+const newPost: Post = {
+    title: 'First post',
+    body: 'body',
+    tags: ['gaming', 'tech'],
+    create_at: new Date(),
+    author: authorOne,
+}
+
+// ---------------
+// as function argument types
+// ---------------
+function creatPost(post: Post): void {
+    console.log(`Created post ${post.title} by ${post.author.name}`);
+}
+creatPost(newPost)
+
+
+
+// ---------------
+// with arrays
+// ---------------
+let posts: Post[] = []
+posts.push(newPost)
