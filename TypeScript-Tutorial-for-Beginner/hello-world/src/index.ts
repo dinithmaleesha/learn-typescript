f2ef13ddf832a65dd71cf1e4e95f67a497c7d36a
Dinith Maleesha <maleesha.live123@gmail.com>
2024-11-12 22:09:29 +0530

    any

--- TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
+++ TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
@@ -1,4 +1,3 @@
-let age: number = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123_456_789;                // no need annotation let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let isPublished: boolean = true
