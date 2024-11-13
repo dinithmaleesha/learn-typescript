06b7dd03bc4e62d52092d3c5d31f2abf70d27624
Dinith Maleesha <maleesha.live123@gmail.com>
2024-11-13 21:55:37 +0530

    arrays

--- TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
+++ TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
@@ -1,3 +1,1 @@
-let sales = 123_456_789;                // no need annotation let sales: number = 123_456_789;
-let course: string = 'TypeScript';
-let isPublished: boolean = true
+let numbers: number[] = [1, 2, 3]
