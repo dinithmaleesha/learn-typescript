b0f0bdcc747430cb4d282b37117eb04e99e5e8b8
Dinith Maleesha <maleesha.live123@gmail.com>
2024-11-16 22:07:12 +0530

    functions

--- TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
+++ TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
@@ -1,2 +1,8 @@
-enum Size { Small, Medium, Large };
-let mySize: Size = Size.Medium
+function calculateTax(income: number): number {
+    return 0;
+}
+
+function taxAmt(income: number, taxYear = 2022): number {
+    return 0;
+}
+taxAmt(10_000);
