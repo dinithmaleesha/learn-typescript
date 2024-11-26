64d92912f4119387f2bfeec1a7f8916ba3463f2d
Dinith Maleesha <maleesha.live123@gmail.com>
2024-11-26 21:13:14 +0530

    Literal Types

--- TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
+++ TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
@@ -1,14 +1,2 @@
-type Draggable = {
-    drag: () => void
-}
-
-type Resizable = {
-    resize: () => void
-}
-
-type UIWidget = Draggable & Resizable;
-
-let textBox: UIWidget = {
-    drag: () => {},
-    resize: () => {}
-}
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
