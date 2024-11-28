6cf43334a0853a9c78c227e4ffcbbd879b835d12
Dinith Maleesha <maleesha.live123@gmail.com>
2024-11-28 22:06:15 +0530

    Optional Chaining

--- TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
+++ TypeScript-Tutorial-for-Beginner/hello-world/src/index.ts
@@ -1,8 +1,11 @@
-function greet(name: string | null) {
-    if(name)
-        console.log(name.toLowerCase());
-    else
-        console.log('Hola');
+type Customer = {
+    birthday: Date
 }
 
-greet(null)
+function getCustomer(id: number): Customer | null {
+    return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(0);
+// Optinal property access operator
+console.log(customer?.birthday);
